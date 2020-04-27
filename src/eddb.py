13cbf342b1809098faa1ba7f04816a9f7328f798
John Kominetz <kominetz@gmail.com>
2020-04-27 03:01:18 -0400

    Updates on WinPC during gameplay
    Renamed path to route in module.
    Split input()s into separate cells to simplify reuse.
    May have broken exercise script since I was unable to run it in VSC due to problems with numpy install.

--- src/eddb.py
+++ src/eddb.py
@@ -3,6 +3,7 @@ import json
 import math
 import os.path
 import tempfile
+import pandas as pd
 
 from urllib.request import urlretrieve
 from enum import Enum
@@ -16,68 +17,17 @@ class Feeds(Enum):
     COMMODITIES = 'https://eddb.io/archive/v6/commodities.json'
     MODULES = 'https://eddb.io/archive/v6/modules.json'
 
-
-def distance(o_name, d_name):
-    o = populated_systems[o_name]
-    d = populated_systems[d_name]
-    return math.ceil(((d['x'] - o['x']) ** 2 + (d['y'] - o['y']) ** 2 + (d['z'] - o['z']) ** 2) ** 0.5)
-
-
-def closest_path(origin, destinations):
-    calculated_path = []
-    d_last = origin
-    d_remaining = destinations.copy()
-    subtotal_distance = 0
-    while len(d_remaining) > 0:
-        d_next = closest(d_last, d_remaining)
-        leg_distance = distance(d_last, d_next)
-        calculated_path.append({'name': d_next, 'distance': leg_distance})
-        d_last = d_next
-        d_remaining.discard(d_last)
-        subtotal_distance += leg_distance
-    return calculated_path, subtotal_distance
-
-
-def resolve_path(path, print_path=False):
-    length = 0
-    last_system = path[0]
-    for next_system in path:
-        dist = distance(last_system, next_system)
-        length += dist
-        if print_path:
-            print("%-24s %5d ly  %4d ly" % (next_system, dist, length))
-        last_system = next_system
-    return length
-
-
-def best_path(waypoints, origin, destination):
-    shortest_path = []
-    shortest_length = -1
-    paths = itertools.permutations(waypoints)
-    for path_tuple in paths:
-        path = list(path_tuple)
-        if origin:
-            path.insert(0, origin)
-        if destination:
-            path.append(destination)
-        length = resolve_path(list(path))
-        if shortest_length < 0 or length < shortest_length:
-            shortest_path = path
-            shortest_length = length
-            # print("%5d ly: %s" % (shortest_length, shortest_path))
-    return shortest_path
-
-
-def closest(origin, destinations):
-    closest_name = origin
-    closest_distance = -1
-    for destination in destinations:
-        destination_distance = distance(origin, destination)
-        if closest_distance == -1 or destination_distance < closest_distance:
-            closest_name = destination
-            closest_distance = destination_distance
-    return closest_name
-
+engineers = [
+    {'system': 'Sirius'},
+    {'system': 'Wyrd'},
+    {'system': 'Kuk'},
+    {'system': 'Alioth'},
+    {'system': 'Wolf 397'},
+    {'system': 'Leesti'},
+    {'system': 'Khun'},
+    {'system': 'Laksak'},
+    {'system': 'Meene'},
+]
 
 def load_feed(feed):
     et_temp_path = os.path.join(tempfile.gettempdir(), 'elite-tools')
@@ -88,7 +38,7 @@ def load_feed(feed):
         print(f'Using existing file "{system_data_path}" for {feed}.')
     else:
         urlretrieve(feed.value, system_data_path)
-        print(f'Downloaded {feed} feed as file "{system_data_path}".')
+        print(f'Downloaded {feed} as file "{system_data_path}".')
 
     feed_data = {}
     feed_file = open(system_data_path)
@@ -98,6 +48,65 @@ def load_feed(feed):
     feed_file.close()
     print("# Populated systems loaded: ", len(feed_data))
     return feed_data
+
+
+def distance(o_name, d_name):
+    o = populated_systems[o_name]
+    d = populated_systems[d_name]
+    return math.ceil(((d['x'] - o['x']) ** 2 + (d['y'] - o['y']) ** 2 + (d['z'] - o['z']) ** 2) ** 0.5)
+
+
+def closest_systems(origin, destinations):
+    closest_name = origin
+    closest_distance = -1
+    for destination in destinations:
+        destination_distance = distance(origin, destination)
+        if closest_distance == -1 or destination_distance < closest_distance:
+            closest_name = destination
+            closest_distance = destination_distance
+    return closest_name
+
+
+def route_len(route, print_route=False):
+    length = 0
+    last_system = route[0]
+    for next_system in route:
+        dist = distance(last_system, next_system)
+        length += dist
+        if print_route:
+            print("%-24s %5d ly  %4d ly" % (next_system, dist, length))
+        last_system = next_system
+    return length
+
+
+def route_to_dataframe(route):
+    route_table = []
+    length = 0
+    last_system = route[0]
+    for next_system in route:
+        dist = distance(last_system, next_system)
+        length += dist
+        route_table.append([next_system, dist, length])
+        last_system = next_system
+    return pd.DataFrame(route_table, columns=['System', 'Leg (ly)', 'Route (ly)'])
+
+def best_route(waypoints, origin, destination, print_choices=False):
+    shortest_route = []
+    shortest_length = -1
+    routes = itertools.permutations(waypoints)
+    for waypoints_route in routes:
+        route = list(waypoints_route)
+        if origin:
+            route.insert(0, origin)
+        if destination:
+            route.append(destination)
+        length = route_len(list(route))
+        if shortest_length < 0 or length < shortest_length:
+            shortest_route = route
+            shortest_length = length
+            if print_choices:
+                print("%5d ly: %s" % (shortest_length, shortest_route))
+    return shortest_route
 
 
 populated_systems = load_feed(Feeds.POPULATED_SYSTEMS)
--- src/elite_trip_planner.py
+++ src/elite_trip_planner.py
@@ -1,17 +1,7 @@
 import eddb
 
 origin_systems = ['EngrCOG', 'Beta Comae Berenices', 'Sol', 'Shinrarta Dezhra']
-engineer_systems = {
-    'Sirius',
-    'Wyrd',
-    'Kuk',
-    'Alioth',
-    'Wolf 397',
-    'Leesti',
-    'Khun',
-    'Laksak',
-    'Meene',
-}
+engineer_systems = {e['name'] for e in eddb.engineers}
 destination_systems = engineer_systems.copy()
 destination_systems.remove('Laksak')
 destination_systems.remove('Meene')
@@ -29,18 +19,19 @@ z_pos /= len(engineer_systems)
 
 print(f"ENGINEER CENTER OF GRAVITY: ({x_pos}, {y_pos}, {z_pos})")
 eddb.populated_systems["EngrCOG"] = {'name': "EngrCOG", 'x': x_pos, 'y': y_pos, 'z': z_pos}
-name = eddb.closest("EngrCOG", eddb.populated_systems.keys())
+name = eddb.closest_systems("EngrCOG", eddb.populated_systems.keys())
 dist = eddb.distance('EngrCOG', name)
 print(f"SYSTEM CLOSEST TO EngrCOG: {name} ({dist} ly)")
 print()
 
-for o in origin_systems:
+for origin in origin_systems:
     print(f"-- ORIGIN SYSTEM: {o} --")
     print()
     print("Distance to Engineers")
-    for d in sorted(engineer_systems):
-        print("%-16s: %4d ly" % (d, eddb.distance(o, d)))
+    for waypoint in sorted(engineer_systems):
+        print("%-16s: %4d ly" % (d, eddb.distance(origin, waypoint)))
     print()
-    print("Best Path Through Destinations")
-    eddb.resolve_path(eddb.best_path(list(destination_systems), origin=o, destination=o), True)
+
+    print("Best Route Through Waypoints")
+    eddb.route_len(eddb.best_route(list(destination_systems), origin=origin, destination=origin), True)
     print()
