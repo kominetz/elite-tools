00f529ed21c6865be1d508e4d31f75322ddf8273
John Kominetz <kominetz@gmail.com>
2020-05-08 19:38:59 -0400

    First tests working
    After almost a day of trying, got unittests working. PyCharm Edu seems to hide test-related menus and run/debug configuration screens.

---
+++ unittests/test_eddb.py
@@ -0,0 +1,18 @@
+from eddb import distance
+
+
+class TestDistance:
+    def test_distance(self):
+        a = {'x': 0, 'y': 0, 'z': 0}
+        b = {'x': 1, 'y': 1, 'z': 1}
+        assert distance(a, b) == round(3 ** 0.5, 1), 'Same start and end point generated non-zero distance.'
+
+    def test_distance_same_points(self):
+        a = {'x': 1, 'y': 10, 'z': 0.1}
+        b = a
+        assert distance(a, b) == 0, 'Same start and end point generated non-zero distance.'
+
+
+class TestAveragePosition:
+    def test_average_position(self):
+        assert False
