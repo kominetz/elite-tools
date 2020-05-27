b82f6b1c25dc676de367692c73b5474cd99a2ee7
John Kominetz <kominetz@gmail.com>
2020-05-27 13:17:07 -0400

    Convert all notebooks to python scripts.

---
+++ notebooks/nearby_systems.py
@@ -0,0 +1,37 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'notebooks'))
+	print(os.getcwd())
+except:
+	pass
+# %%
+import eddb
+eddb.load_feeds()
+
+
+# %%
+systems = input("Systems: ")
+origin = eddb.center(systems)
+
+
+# %%
+radius = int(input("Radius (ly): "))
+if radius == 0:
+    radius = max(eddb.distance(origin, s.strip()) for s in systems.split(","))
+radius
+
+
+# %%
+nearby_systems = eddb.query_nearby_systems(origin, radius)
+system_summaries = eddb.system_survey_report(nearby_systems, origin)
+system_summaries
+
+
+# %%
+system_summaries.to_clipboard(header=None, index=False)
+
+
---
+++ notebooks/system_summaries.py
@@ -0,0 +1,32 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'notebooks'))
+	print(os.getcwd())
+except:
+	pass
+# %%
+import eddb
+eddb.load_feeds()
+
+
+# %%
+reference_system = input("Reference System: ")
+
+
+# %%
+systems = [wp.strip() for wp in input("Systems to summarize: ").split(',')]
+
+
+# %%
+system_summaries = eddb.system_survey_report(systems, reference_system)
+system_summaries.sort_values(by='Name')
+
+
+# %%
+system_summaries.to_clipboard(header=None, index=False)
+
+
